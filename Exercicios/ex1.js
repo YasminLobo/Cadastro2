6ea83a38f2cdead16308fb13278cfdfd8cbb4882
Lana Araújo <lana.academico2@gmail.com>
2023-03-17 15:19:18 -0300

    img

---
+++ Exercicios/ex1.js
@@ -0,0 +1,7 @@
+ var idade
+
+ if(idade <=18){
+    alert("Você não pode comprar este ingresso", você tem  + idade) 
+} else if(idade >=18){
+    alert("Você pode comprar este ingresso, você tem " + idade)
+}
